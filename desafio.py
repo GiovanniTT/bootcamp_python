f14c0731314e3e04110342a1b10bdf22133a2a6f
Giovanni Torres <gi-torres1@hotmail.com>
2026-01-26 11:45:08 -0300

    Primeiro Desafio

--- desafio.py
+++
@@ -1,6 +0,0 @@
-constante_bonus = 1000
-nome = input("Digite seu nome: ")
-salario = float(input("Digite seu salário: "))
-bonus = float(input("Digite o bônus recebido: "))
-salario_total = constante_bonus + salario * bonus
-print(f"Olá {nome}, o seu bonus foi de R$ {salario_total:.2f}")
